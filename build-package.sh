eebe7d0a790fbdb801ad486637b862ebcd5d882c
Rakib Ansary <rakibansary@gmail.com>
2022-06-13 17:18:50 +0600

    ci: fix typo and give exec perms to deploy.sh

--- build-package.sh
+++ build-package.sh
@@ -11,19 +11,17 @@ fi
 VER=`date "+%Y%m%d%H%M"`
 
 automated_testing_processor_cdpackage() {
-    AWS_CD_PACKAGE_NAME="${APPNAME}-${VER}.zip"
-    PACKAGE_LOCATION="${APPNAME}"
+    AWS_CD_PACKAGE_NAME="${APP_NAME}-${VER}.zip"
+    PACKAGE_LOCATION="${APP_NAME}"
 
     rm -rf $PACKAGE_LOCATION
-
-    npm i
     mkdir $PACKAGE_LOCATION
 
     cp .env $PACKAGE_LOCATION/
     cp -r scripts $PACKAGE_LOCATION/
     cp -r src node_modules config $PACKAGE_LOCATION/
 
-    zip -j $AWS_CD_PACKAGE_NAME $PACKAGE_LOCATION/*
+    zip -r $AWS_CD_PACKAGE_NAME $PACKAGE_LOCATION/
 }
 
 automated_testing_processor_cdpackage
